9cbb2fd5ebfe8243eb31d6baae9e4a25cf4abd18
Trevor Lovell <trevor.lovell@makeinfinite.com>
2026-02-06 23:50:12 +0000

    feat: proof-of-concept

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Nix shell for developing sxt-proof-of-sql";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in {
+        devShells.default = with pkgs;
+          (mkShell.override {stdenv = gcc13Stdenv;}) {
+            buildInputs = [
+              rust-bin.stable.latest.default
+            ];
+          };
+      }
+    );
+}
