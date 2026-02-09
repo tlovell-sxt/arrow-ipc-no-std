b9534e8ead4315b7b5631e8ab0a47950f08b1e0a
Trevor Lovell <trevor.lovell@makeinfinite.com>
2026-02-09 19:08:35 +0000

    feat: auto-generate arrow flatbuffers on builds

--- arrow-ipc-no-std/build.rs
+++ arrow-ipc-no-std/build.rs
@@ -1,13 +1,12 @@
 use std::path::Path;
 
-use flatc_rust;
-
 fn main() {
-    // flatc_rust::run(flatc_rust::Args {
-    //     inputs: &[Path::new("flatbuffers/Message.fbs")],
-    //     out_dir: Path::new("src/generated.rs"),
-    //     extra: &["--gen-all"],
-    //     ..Default::default()
-    // })
-    // .expect("flatc");
+    println!("cargo:rerun-if-changed=flatbuffers/*.fbs");
+    flatc_rust::run(flatc_rust::Args {
+        inputs: &[Path::new("flatbuffers/Message.fbs")],
+        out_dir: Path::new("target/flatbuffers/"),
+        extra: &["--gen-all"],
+        ..Default::default()
+    })
+    .expect("flatc");
 }
--- arrow-ipc-no-std/src/lib.rs
+++ arrow-ipc-no-std/src/lib.rs
@@ -1,4 +1,15 @@
 #![no_std]
 
+#[expect(
+    mismatched_lifetime_syntaxes,
+    non_snake_case,
+    non_camel_case_types,
+    unused_imports,
+    clippy::missing_safety_doc,
+    clippy::extra_unused_lifetimes,
+    clippy::derivable_impls,
+    clippy::doc_lazy_continuation
+)]
+#[path = "../target/flatbuffers/Message_generated.rs"]
 mod generated;
 pub use generated::*;
